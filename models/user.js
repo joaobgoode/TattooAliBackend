3f94d19b90b5a636d4dc7352cc2eff360061523a
Teresa Campos <166440846+teresaccampos@users.noreply.github.com>
2025-10-07 19:50:14 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -27,9 +27,65 @@ const User = sequelize.define('User', {
   telefone: DataTypes.STRING,
   whatsapp: DataTypes.STRING,
   instagram: DataTypes.STRING,
-  especialidades: DataTypes.STRING,
-  foto: DataTypes.STRING,
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/database');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  sobrenome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  bio: {
+    type: DataTypes.STRING,
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  endereco: DataTypes.STRING,
+  telefone: DataTypes.STRING,
+  whatsapp: DataTypes.STRING,
+  instagram: DataTypes.STRING,
+  foto: {
+    type: DataTypes.STRING,
+    get() {
+      const rawValue = this.getDataValue('foto');
+      return rawValue ? `${process.env.PUBLIC_BUCKET_URL}/${rawValue}` : null;
+    }
+  },
+  
   email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8, 100] // Senha deve ter entre 8 e 100 caracteres
+    }
+  }
+});
+
+User.beforeCreate(async (user, options) => {
+  user.senha = await bcrypt.hash(user.senha, 10);
+});
+
+module.exports = User;  email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
