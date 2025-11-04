08909c412f4455b6ba426129cb99760570eaf919
FabricioCrv <fabriciogarciacarvalho87@hotmail.com>
2025-11-04 19:50:30 -0300

    Recuperar e alterar a senha

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,7 @@ const userController = require('../controllers/userController');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/recuperar-senha', userController.recoverPassword);
+router.post('/alterar-senha', userController.alterarSenha);
+
 
 module.exports = router;
