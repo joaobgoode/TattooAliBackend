31b153c0dcd729125ec092966163a11dda62097d
Fabricio <fabriciogarciacarvalho87@hotmail.com>
2026-04-11 15:51:54 -0300

    testando a role de perfil

---
+++ test/perfil.test.js
@@ -0,0 +1,30 @@
+const app = require("../app.js")
+const request = require('supertest');
+
+TEST_EMAIL = process.env.TEST_EMAIL
+TEST_PASSWORD = process.env.TEST_PASSWORD
+
+
+
+describe("Checagem do role do usuario", function () {
+    it('testando se o usuario aparece com o role correto (tatuador, cliente ou admin)', async () => {
+        const loginRes = await request(app)
+            .post('/api/user/login')
+            .send({email: TEST_EMAIL, senha: TEST_PASSWORD});
+        
+        expect(loginRes.statusCode).toBe(200);
+        expect(loginRes.body).toHaveProperty('token');
+
+        const token = loginRes.body.token;
+
+        const perfilRes = await request(app)
+            .get('/api/perfil/')
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(perfilRes.statusCode).toBe(200);
+        expect(perfilRes.body).toHaveProperty('role');
+
+        expect(['cliente', 'tatuador', 'admin']).toContain(perfilRes.body.role)
+        console.log(loginRes.body)
+    })
+})
